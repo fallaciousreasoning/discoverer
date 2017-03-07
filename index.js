05cb2fa4f89bf3c179ebe1020bd0f3550224c0fd
Jay Harris <jay.harris@outlook.co.nz>
2017-03-07 17:02:00 +1300

    Adds a simple system for getting spotify track ids

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 let lastfm = require("./lastfm.js");
+let spotify = require("./spotify-helper.js");
+
 let Finder = require("./finder.js");
 
 let jsonfile = require("jsonfile");
@@ -29,12 +31,23 @@ function findTracks() {
                     name: "Streetlight Manifesto"
                 }
             }
-        ]
+        ],
+        maxDepth: 2,
+        burnUsedArtists: true,
+        limit: 30
     });
 
     finder.generate().then(() => {
-        console.log(finder.result);
+        jsonfile.writeFileSync("playlist.json", finder.result);
     });
 }
 
-findTracks();
+function matchTracks() {
+    let playlist = jsonfile.readFileSync("playlist.json");
+    let spotifyTracks = spotify.getTrackIds(playlist)
+        .then(result => {
+            jsonfile.writeFileSync("spotify-playlist.json", result);
+        });
+}
+
+matchTracks();
---
+++ spotify-helper.js
@@ -0,0 +1,35 @@
+const Spotify = require("spotify-web-api-node");
+const config = require("config.json")("./spotify-config.json");
+
+const spotify = new Spotify({
+  clientId : config.clientId,
+  clientSecret : config.clientSecret,
+  redirectUri : config.callbackUrl
+});
+
+function getTrack(playlist, i, tracks) {
+    if (i >= playlist.length) return;
+    let track = playlist[i];
+
+    return spotify.searchTracks(track.name + " " + track.artist.name)
+        .then(result => {
+            if (result.body.tracks.items.length === 0) return;
+
+            tracks.push(result.body.tracks.items[0]);
+        }, error => console.log(error))
+        .then(() => {
+            return getTrack(playlist, i + 1, tracks);
+        });
+}
+
+module.exports = {
+    getTrackIds: (playlist) => {
+        var ids = [],
+            i = 0;
+
+        return getTrack(playlist, 0, ids)
+            .then(() => {
+                return ids;
+            });
+    }
+}
