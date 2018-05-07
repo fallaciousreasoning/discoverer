2969b954277b81d5e8ff8a4af8aa2265921a2f05
Jay Harris <jay.harris@outlook.co.nz>
2018-05-07 20:56:49 +1000

    Moves the linker into the saga (which is cool)

--- src/sagas/link.ts
+++ src/sagas/link.ts
@@ -1,5 +1,7 @@
-import { all, takeEvery, select } from "redux-saga/effects";
+import { all, takeEvery, select, put } from "redux-saga/effects";
 import { ActionType, LinkToSpotify, actionCreators } from "src/store/actions";
+
+import * as Spotify from 'spotify-web-api-js';
 
 import { Discoverer } from "src/services/discoverer";
 import { ApplicationState } from "src/store";
@@ -8,15 +10,40 @@ import { store } from 'src/index';
 import { DiscoverTrack } from "src/store/generationStore";
 import Linker from "../services/linker";
 import { AuthorizationToken } from "../store/authorizationStore";
+import { getArtistName } from "../services/lastfm";
 
 let discoverer: Discoverer;
 
 function* link(action: LinkToSpotify) {
     const generated: DiscoverTrack[] = yield select((state: ApplicationState) => state.generation.generated);
     const token: AuthorizationToken = yield select((state: ApplicationState) => state.token);
-    const linker = new Linker(token, (song, id) => store.dispatch(actionCreators.setSpotifyId(song, id)));
+
+    const client = new Spotify();
+    client.setAccessToken(token.access_token);
     
-    linker.link(generated);
+    const trackUris: string[] = [];
+    for (const song of generated) {
+        const response = yield client.searchTracks(`${song.name} ${getArtistName(song)}`);
+
+        if (!response.tracks || !response.tracks.items || !response.tracks.items.length) {
+            continue;
+        }
+
+        const spotifyTrack = response.tracks.items[0];
+        trackUris.push(spotifyTrack.uri);
+        yield put(actionCreators.setSpotifyId(song, spotifyTrack.uri));
+    }
+
+    const currentUser = yield client.getMe();
+    const playlist = yield client.createPlaylist(currentUser.id, {
+        name: "__test__"
+    });
+
+    const step = 100;
+    for (let i = 0; i < trackUris.length; i += step) {
+        // Spotify only lets you add 100 track ids at a time
+        const result = yield client.addTracksToPlaylist(currentUser.id, playlist.id, trackUris.slice(i, Math.min(i + step, trackUris.length)));
+    }
 }
 
 export default function* () {
