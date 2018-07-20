5ef32d5984840e4c8f6e7dea0b5b6119b6556ba4
Jay Harris <jay.harris@outlook.co.nz>
2018-07-20 18:40:11 +1000

    Stubs a data context

---
+++ src/services/dataContext.ts
@@ -0,0 +1,41 @@
+import * as localforage from 'localforage';
+import { Track } from 'src/store/trackStore';
+
+const trackPrefix = "track";
+
+localforage.config({ 
+    description: 'Persist information',
+    name: 'discoverer',
+    driver: [localforage.INDEXEDDB, localforage.LOCALSTORAGE]
+});
+
+const cache = {};
+
+async function setItem<T>(key: string, item: T) {
+    const itemToCache = { ...await getItem(key), ...item as any }
+
+    cache[key] = itemToCache;
+    await localforage.setItem(key, itemToCache);
+
+    return itemToCache;
+}
+
+async function getItem(key: string) {
+    const cached = cache[key];
+    if (cached) return cached;
+
+    const result = await localforage.getItem(key);
+    cache[key] = result;
+
+    return result;
+}
+
+export async function setTrack(track: Track) {
+    const key = `${trackPrefix} ${track.id}`;
+    return await setItem(key, track);
+}
+
+export async function getTrack(id: string) {
+    const key = `${trackPrefix} ${id}`;
+    return await getItem(key);
+}
