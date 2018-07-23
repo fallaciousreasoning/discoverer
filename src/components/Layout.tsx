51317461c94715192dabb0c49d91d6597a3e0aac
Jay Harris <jay.harris@outlook.co.nz>
2018-07-23 20:53:39 +1000

    Adds an AppBar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,21 @@
+import { IconButton, Toolbar, Typography } from '@material-ui/core';
 import AppBar from '@material-ui/core/AppBar';
+import MenuIcon from '@material-ui/icons/Menu';
 import * as React from 'react';
 
 export default class Layout extends React.Component {
     public render() {
         return <div>
-            <AppBar></AppBar>
+            <AppBar>
+                <Toolbar variant='dense'>
+                    <IconButton color='inherit'>
+                        <MenuIcon/>
+                    </IconButton>
+                    <Typography variant="title" color="inherit">
+                        Discoverer
+                    </Typography>
+                </Toolbar>
+            </AppBar>
             {this.props.children}
         </div>
     }
